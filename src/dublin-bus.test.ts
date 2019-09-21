2c55461d45ab1190643a0bc4f5a949210c6a60b2
Luciano <lucianomammino@gmail.com>
2019-09-21 17:31:08 +0100

    Added test for get stop real time info

--- src/dublin-bus.test.ts
+++ src/dublin-bus.test.ts
@@ -7,11 +7,14 @@ import * as dublinBus from './dublin-bus'
 
 const wsdlResponse = readFileSync(join(__dirname, 'fixtures', 'dublin-bus-wsdl.xml'))
 const allDestinationsResponseData = readFileSync(join(__dirname, 'fixtures', 'dublin-bus-get-all-destinations.xml'))
+const realTimeInfoData = readFileSync(join(__dirname, 'fixtures', 'dublin-bus-get-real-time-stop-data.xml'))
 
+// mock WSDL request
 nock('http://rtpi.dublinbus.ie')
   .get('/DublinBusRTPIService.asmx?WSDL')
   .reply(200, wsdlResponse)
 
+// mock get all destinations request
 nock('http://rtpi.dublinbus.ie', {
   reqheaders: {
     SOAPAction: '"http://dublinbus.ie/GetAllDestinations"'
@@ -21,7 +24,22 @@ nock('http://rtpi.dublinbus.ie', {
   .once()
   .reply(200, allDestinationsResponseData)
 
+// mock get real time info request
+nock('http://rtpi.dublinbus.ie', {
+  reqheaders: {
+    SOAPAction: '"http://dublinbus.ie/GetRealTimeStopData"'
+  }
+})
+  .post('/DublinBusRTPIService.asmx')
+  .once()
+  .reply(200, realTimeInfoData)
+
 test('It should get all stops', async function () {
   const stops = await dublinBus.getStops()
   expect(stops).toMatchSnapshot()
+})
+
+test('It should get real time information for a stop', async function () {
+  const realTimeInfo = await dublinBus.getRealTimeInfoForStop(22)
+  expect(realTimeInfo).toMatchSnapshot()
 })
