e169b6bba55d997b86fc6eb4fea3d86bfc884a74
Luciano <lucianomammino@gmail.com>
2019-09-22 10:23:00 +0100

    switching to ubuntu containers again and using cobertura coverage reports

--- jest.config.js
+++ jest.config.js
@@ -32,12 +32,13 @@ module.exports = {
   // ],
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: [
+    'json',
+    'text',
+    'lcov',
+    'clover',
+    'cobertura'
+  ],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: null,
