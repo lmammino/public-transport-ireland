a4a6b7c1337aab8afbbf0ca3e3b519209c12948a
Luciano <lucianomammino@gmail.com>
2019-09-22 09:44:12 +0100

    Azure pipelines xunit integration

--- jest.config.js
+++ jest.config.js
@@ -97,7 +97,10 @@ module.exports = {
   // projects: null,
 
   // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
+  reporters: [
+    'default',
+    'jest-junit'
+  ],
 
   // Automatically reset mock state between every test
   // resetMocks: false,
