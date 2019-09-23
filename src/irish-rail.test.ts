db8d5dd81bfc4a0924062119fe42f832b7fe786d
Luciano <lucianomammino@gmail.com>
2019-09-23 09:13:12 +0100

    Added getRealTimeInfo in irish-rail

--- src/irish-rail.test.ts
+++ src/irish-rail.test.ts
@@ -6,6 +6,7 @@ import nock from 'nock'
 import irishRail from './irish-rail'
 
 const allStationsResponseData = readFileSync(join(__dirname, 'fixtures', 'irish-rail', 'get-all-stations.xml'))
+const realTimeInfoResponseData = readFileSync(join(__dirname, 'fixtures', 'irish-rail', 'get-real-time-info.xml'))
 
 // mock get all stations request
 nock('http://api.irishrail.ie')
@@ -13,7 +14,19 @@ nock('http://api.irishrail.ie')
   .once()
   .reply(200, allStationsResponseData)
 
+nock('http://api.irishrail.ie')
+  .get('/realtime/realtime.asmx/getStationDataByCodeXML')
+  .query({ StationCode: 'SEAPT' })
+  .once()
+  .reply(200, realTimeInfoResponseData)
+
 test('It should get all stations', async function () {
   const stations = await irishRail.getStations()
   expect(stations).toMatchSnapshot()
+})
+
+test('It should get real time informationf for a station', async function () {
+  const stationCode = 'SEAPT'
+  const trains = await irishRail.getRealTimeInfo(stationCode)
+  expect(trains).toMatchSnapshot()
 })
--- src/irish-rail.ts
+++ src/irish-rail.ts
@@ -6,22 +6,72 @@ import { Parser } from 'xml2js'
 interface AllStationsResponse {
   readonly ArrayOfObjStation: {
     readonly objStation: Array<{
-      StationDesc: Array<string>
-      StationAlias: Array<string>
-      StationLatitude: Array<string>
-      StationLongitude: Array<string>
-      StationCode: Array<string>
-      StationId: Array<string>
+      readonly StationDesc: Array<string>
+      readonly StationAlias: Array<string>
+      readonly StationLatitude: Array<string>
+      readonly StationLongitude: Array<string>
+      readonly StationCode: Array<string>
+      readonly StationId: Array<string>
+    }>
+  }
+}
+
+interface AllTrainsResponse {
+  readonly ArrayOfObjStationData: {
+    readonly objStationData: Array<{
+      readonly Servertime: Array<string>
+      readonly Traincode: Array<string>
+      readonly Stationfullname: Array<string>
+      readonly Stationcode: Array<string>
+      readonly Querytime: Array<string>
+      readonly Traindate: Array<string>
+      readonly Origin: Array<string>
+      readonly Destination: Array<string>
+      readonly Origintime: Array<string>
+      readonly Destinationtime: Array<string>
+      readonly Status: Array<string>
+      readonly Lastlocation: Array<string>
+      readonly Duein: Array<string>
+      readonly Late: Array<string>
+      readonly Exparrival: Array<string>
+      readonly Expdepart: Array<string>
+      readonly Scharrival: Array<string>
+      readonly Schdepart: Array<string>
+      readonly Direction: Array<string>
+      readonly Traintype: Array<string>
+      readonly Locationtype: Array<string>
     }>
   }
 }
 
 interface Station {
-  id?: number
-  code?: string
-  name?: string
-  latitude?: number
-  longitude?: number
+  /** The station id (e.g. 228) */
+  id: number
+  /** The station code (e.g. "BFSTC") */
+  code: string
+  /** The station name (e.g. "Belfast") */
+  name: string
+  /** The station latitude (e.g. 54.6123) */
+  latitude: number
+  /** The station longitude (e.g. -5.91744) */
+  longitude: number
+}
+
+interface Train {
+  code: string
+  origin: string
+  destination: string
+  originTime: string
+  destinationTime: string
+  status: string
+  arrivingInMinutes: number
+  minutesLate: number,
+  expectedArrivalTime: string,
+  expectedDepartureTime: string,
+  scheduledArrivalTime: string,
+  scheduledDepartureTime: string,
+  direction: string,
+  trainType: string
 }
 
 function parseXml (xmlString : string) : Promise<any> {
@@ -37,6 +87,9 @@ function parseXml (xmlString : string) : Promise<any> {
   })
 }
 
+/**
+ * Get information about all available train stations
+ */
 export async function getStations () : Promise<Array<Station>> {
   const response = await got('http://api.irishrail.ie/realtime/realtime.asmx/getAllStationsXML')
   const document : AllStationsResponse = await parseXml(response.body)
@@ -52,6 +105,39 @@ export async function getStations () : Promise<Array<Station>> {
   return stations
 }
 
+/**
+ * Get information about trains arriving at a given station
+ */
+export async function getRealTimeInfo (stationCode: string): Promise<Array<Train>> {
+  const response = await got('http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML', {
+    query: {
+      StationCode: stationCode
+    }
+  })
+
+  const document : AllTrainsResponse = await parseXml(response.body)
+
+  const trains = document.ArrayOfObjStationData.objStationData.map((train) => ({
+    code: train.Traincode[0].trim(),
+    origin: train.Origin[0].trim(),
+    destination: train.Destination[0].trim(),
+    originTime: train.Origintime[0].trim(),
+    destinationTime: train.Destinationtime[0].trim(),
+    status: train.Status[0].trim(),
+    arrivingInMinutes: Number(train.Duein[0].trim()),
+    minutesLate: Number(train.Late[0].trim()),
+    expectedArrivalTime: train.Exparrival[0].trim(),
+    expectedDepartureTime: train.Expdepart[0].trim(),
+    scheduledArrivalTime: train.Scharrival[0].trim(),
+    scheduledDepartureTime: train.Schdepart[0].trim(),
+    direction: train.Direction[0].trim(),
+    trainType: train.Traintype[0].trim()
+  }))
+
+  return trains
+}
+
 export default {
-  getStations
+  getStations,
+  getRealTimeInfo
 }
