bed3ed47849583b85b39f366e91ff49c296349ef
Luciano <lucianomammino@gmail.com>
2019-09-29 12:04:21 +0100

    Fixed top level exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-import * as dublinBus from './dublin-bus'
-export default dublinBus
+import * as _dublinBus from './dublin-bus'
+import * as _irishRail from './irish-rail'
+import * as _luas from './luas'
+
+export default {
+  dublinBus: _dublinBus,
+  irishRail: _irishRail,
+  luas: _luas
+}
+
+export const dublinBus = _dublinBus
+export const irishRail = _irishRail
+export const luas = _luas
